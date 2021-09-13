5907fae93d24df3c475084b84f9450374872b893
nlawliet6 <nlawliet6@gmail.com>
2021-09-13 14:08:53 -0400

    flixter 9/13 update

---
+++ flixster/flixster/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieCell
+//
+//  Created by MacBook Pro on 9/7/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsisLabel: UILabel!
+    @IBOutlet var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
