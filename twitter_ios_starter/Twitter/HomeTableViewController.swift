5471de770a55e556d082164cc82ca42a279d896e
nlawliet6 <nlawliet6@gmail.com>
2021-09-27 02:23:05 -0400

    updated TipCalculator & Twitter

---
+++ twitter_ios_starter/Twitter/HomeTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by MacBook Pro on 9/27/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+    @IBAction func onLogOut(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
---
+++ twitter_ios_starter/Twitter/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  LoginViewController
+//
+//  Created by MacBook Pro on 9/23/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl,
+            success:loginSuccess,
+            failure: {(Error) in print("")})
+    }
+    
+    func loginSuccess(){
+        let destination = "loginToHome"
+        performSegue(withIdentifier: destination , sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
