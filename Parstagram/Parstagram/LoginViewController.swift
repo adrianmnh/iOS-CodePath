871698dcf09770b6eae8fb84cb610e0f7df8bc4e
nlawliet6 <nlawliet6@gmail.com>
2021-10-14 18:54:30 -0400

    rebase

---
+++ Parstagram/Parstagram/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Oberon on 10/7/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        image.
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parstagram/Pods/Target Support Files/Pods-Parstagram-ParstagramUITests/Pods-Parstagram-ParstagramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Parstagram_ParstagramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Parstagram_ParstagramUITests
+@end
